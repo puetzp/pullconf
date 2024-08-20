c5392e3e6e9e5904fdf0d2ebc43949ad335fb336
puetzp <ppuetz@protonmail.com>
2024-08-20 17:29:31 +0200

    Add `common::resources::apt::preference`

---
+++ common/src/resources/apt/preference.rs
@@ -0,0 +1,80 @@
+use crate::{Ensure, ResourceMetadata};
+use serde::{de::Error, Deserialize, Deserializer, Serialize};
+use std::fmt;
+use std::ops::Deref;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Parameters {
+    pub ensure: Ensure,
+    pub name: Name,
+    pub package: String,
+    pub pin: String,
+    pub priority: String,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct Relationships {
+    pub requires: Vec<ResourceMetadata>,
+}
+
+/// The name of a preference doubles as the name for a file created
+/// in `/etc/apt/preferences.d`. As such the restrictions on file names
+/// described in `man apt_preferences` apply to `Name`.
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct Name(String);
+
+impl FromStr for Name {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some(ref c) = s
+            .chars()
+            .find(|c| !(c.is_ascii_alphanumeric() || *c == '_' || *c == '-' || *c == '.'))
+        {
+            return Err(format!(
+                "apt preference name `{}` contains invalid character `{}`",
+                s, c
+            ));
+        }
+
+        Ok(Self(s.to_owned()))
+    }
+}
+
+impl<'de> Deserialize<'de> for Name {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let v = String::deserialize(deserializer)?;
+
+        Self::from_str(&v).map_err(Error::custom)
+    }
+}
+
+impl From<&Name> for Name {
+    fn from(name: &Name) -> Self {
+        name.clone()
+    }
+}
+
+impl Deref for Name {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_str()
+    }
+}
+
+impl fmt::Display for Name {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&*self.0, f)
+    }
+}
+
+impl Name {
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
